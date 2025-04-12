ccb2cfe93e3d62e9a07fdfda6e1e4127a572bb62
SARDORALOYEV <aloyevsardor30@gmail.com>
2025-04-13 01:55:51 +0500

    style qoshildi

--- src/App.jsx
+++ src/App.jsx
@@ -13,10 +13,13 @@ const App = () => {
     }
   }
   return (
-    <div>
-      <h1>{son}</h1>
-      <button onClick={plus}>Plus</button>
-      <button onClick={minus}>Minus</button>
+    <div className='w-full h-[100vh] flex justify-center items-center flex-col'>
+      <h1 className="text-4xl font-bold text-center text-blue-600 my-4">{son}</h1>
+<div className="flex gap-3 justify-center">
+<button className="bg-green-500 text-white px-4 py-2 rounded mr-2" onClick={plus}>Plus</button>
+<button className="bg-red-500 text-white px-4 py-2 rounded" onClick={minus}>Minus</button>
+
+</div>
     </div>
   )
 }
